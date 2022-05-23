467ed1dc68fb18fa0a4ff0bf0b083a72d1613cab
brandon-admire-osu <admireb@oregonstate.edu>
2022-05-22 22:13:34 -0700

    spaggeti code

--- preprocessing.py
+++ preprocessing.py
@@ -1,32 +1,137 @@
 import re
 
+
+
 if __name__ == "__main__":
-    training_set_raw = [re.sub(r'[^a-zA-Z0-9 ]', '',(line.lower())) for line in open("./trainingSet.txt")]
+
+    # Classification
+    training_set_raw = [
+        re.sub(r"[^a-zA-Z0-9 ]", "", (line.lower()))
+        for line in open("./trainingSet.txt")
+    ]
 
     training_set_token = [line.split() for line in training_set_raw]
 
-    test_set_raw = [re.sub(r'[^a-zA-Z0-9 ]', '',(line.lower())) for line in open("./testSet.txt")]
+    test_set_raw = [
+        re.sub(r"[^a-zA-Z0-9 ]", "", (line.lower())) for line in open("./testSet.txt")
+    ]
 
     test_set_token = [line.split() for line in test_set_raw]
 
-    vocab = dict()
+    vocab = {"good":dict(),"bad":dict()}
+    test_vocab = {"good":dict(),"bad":dict()}
 
     for sentence in training_set_token:
-        if sentence[-1]:
+        if int(sentence[-1]):
             for word in sentence[:-1]:
-                if word in vocab:
-                    vocab[word][-1] = 1
+                if word in vocab["good"]:
+                    vocab["good"][word] += 1
                 else:
-                    vocab[word] = [0,1]
+                    vocab["good"][word] = 1
         else:
-                if word in vocab:
-                    vocab[word][0] = 1
+            for word in sentence[:-1]:
+                if word in vocab["bad"]:
+                    vocab["bad"][word] += 1
                 else:
-                    vocab[word] = [1,0]
+                    vocab["bad"][word] = 1
 
-    sorted(vocab)
+    for sentence in test_set_token:
+        if int(sentence[-1]):
+            for word in sentence[:-1]:
+                if word in test_vocab["good"]:
+                    test_vocab["good"][word] += 1
+                else:
+                    test_vocab["good"][word] = 1
+        else:
+            for word in sentence[:-1]:
+                if word in test_vocab["bad"]:
+                    test_vocab["bad"][word] += 1
+                else:
+                    test_vocab["bad"][word] = 1
 
-    for word in vocab:
-        if vocab[word] == [1,1]:
-            print(word,vocab[word])
+    all_words = sorted(set(list(vocab["bad"].keys()) + list(vocab["good"].keys())+list(test_vocab["bad"].keys()) + list(test_vocab["good"].keys())))
 
+    # Sort it
+    vocab["good"] = {key:vocab["good"][key] for key in sorted(vocab["good"].keys())}
+    vocab["bad"] = {key:vocab["bad"][key] for key in sorted(vocab["bad"].keys())}
+
+
+    line_1 = ""
+    line_2 = ""
+    line_3 = ""
+
+    for word in all_words:
+        line_1 += word+","
+        
+        if word in vocab["bad"] or word in test_vocab["bad"]:
+            line_2 += "1,"
+        else:
+            line_2 += "0,"
+
+        if word in vocab["good"] or word in test_vocab["good"]:
+            line_3 += "1,"
+        else:
+            line_3 += "0,"
+
+    line_1 += "classlabel\n"
+    line_2 += "0\n"
+    line_3 += "1\n"
+
+    target = open("./test_out","w")
+    target.write(line_1)
+    target.write(line_2)
+    target.write(line_3)
+    target.close()
+
+
+    # Training
+
+    # Probablity of class variable
+    CV_total = (len(vocab["good"])+len(vocab["bad"]))
+    CV_true = len(vocab["good"])
+    CV_false = len(vocab["bad"])
+    pCV_false = CV_false/CV_total
+    pCV_true = CV_true/CV_total
+
+    # Calculate probability of parameters
+
+
+    predictions = []
+
+    for sentence in test_set_token:
+        current_good = 1
+        current_bad = 1
+        unknown = []
+        for word in sentence[:-1]:
+            if word in vocab:
+                fGf = (CV_total - vocab["bad"][word])/CV_false # false given false
+                fGt = (CV_total - vocab["bad"][word])/CV_true # false given true
+                tGf = (vocab["bad"][word])/CV_false # true given false
+                tGt = (vocab["bad"][word])/CV_true # true given true
+                current_good = current_good * (tGt + fGt)
+                current_bad = current_bad * (tGf + fGf)
+            else:
+                unknown.append(word)
+        
+        if len(unknown) == len(sentence[:-1]):
+            predictions.append(-1)
+        else:
+            current_bad = current_bad*pCV_false
+            current_good = current_good*pCV_true
+
+            if current_good > current_bad:
+                predictions.append(1)
+            else:
+                predictions.append(0)
+
+    correct = 0
+    wrong = 0
+
+    for i in range(len(predictions)):
+        if predictions[i] == -1:
+            continue
+        elif predictions[i] == int(test_set_token[i][-1]):
+            correct += 1
+
+    print(correct,len(test_set_token))
+    print(correct/len(test_set_token))

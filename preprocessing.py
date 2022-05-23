124a262a4ea4e8b66361ab5bd629aaab278bff68
Brandon Admire <admireb@oregonstate.edu>
2022-05-22 18:46:19 -0700

    started

--- preprocessing.py
+++ preprocessing.py
@@ -9,24 +9,5 @@ if __name__ == "__main__":
 
     test_set_token = [line.split() for line in test_set_raw]
 
-    vocab = dict()
-
-    for sentence in training_set_token:
-        if sentence[-1]:
-            for word in sentence[:-1]:
-                if word in vocab:
-                    vocab[word][-1] = 1
-                else:
-                    vocab[word] = [0,1]
-        else:
-                if word in vocab:
-                    vocab[word][0] = 1
-                else:
-                    vocab[word] = [1,0]
-
-    sorted(vocab)
-
-    for word in vocab:
-        if vocab[word] == [1,1]:
-            print(word,vocab[word])
+    
 
